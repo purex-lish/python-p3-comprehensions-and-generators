bfcda1f9db32c807193c3016bde93b1ba0303af2
Purity Wambua <puritywamb@gmail.com>
2024-08-30 14:52:51 +0300

    comprehensions and gen

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    '''Returns a list of all even elements from the given list of integers.'''
+    return [x for x in num_list if x % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    '''Returns a list of sentences with an exclamation mark at the end of each sentence.'''
+    return [sentence + '!' for sentence in sentence_list]
